cf9ff0c71d35266c42587c6915be6942ee43dc54
Santhiya <santhiyaps@gmail.com>
2021-06-05 18:18:20 -0700

    Running Sum

---
+++ src/main/java/week1/RunningSum.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class RunningSum {
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            nums[i]+=nums[i-1];
+        }
+        return nums;
+    }
+}
